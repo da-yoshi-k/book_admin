7165ff0667a1b828bfc98f7793821e647894ef03
da-yoshi-k <hey234234s@gmail.com>
2021-03-31 18:45:25 +0900

    fix rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_062522) do
-
+ActiveRecord::Schema.define(version: 20_210_331_062_522) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.string "penname"
